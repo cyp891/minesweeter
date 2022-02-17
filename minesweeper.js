7529c573112209f6ac4b41354ca7bc0e343bc4b7
cyp891 <62851740+cyp891@users.noreply.github.com>
2022-02-17 15:13:41 +0200

    inițial comit

---
+++ minesweeper.js
@@ -0,0 +1,121 @@
+export const TILE_STATUSES = {
+  HIDDEN: "hidden",
+  NUMBER: "number",
+  MARKED: "marked",
+  MINE: "mine",
+};
+
+export function createBoard(boardSize, numberOfMines) {
+  const board = [];
+  const minePosition = getMinePosition(boardSize, numberOfMines);
+
+  for (let x = 0; x < boardSize; x++) {
+    const row = [];
+    for (let y = 0; y < boardSize; y++) {
+      const element = document.createElement("div");
+      element.dataset.status = TILE_STATUSES.HIDDEN;
+
+      const tile = {
+        element,
+        x,
+        y,
+        mine: minePosition.some(positionMatch.bind(null, { x, y })),
+        get status() {
+          return this.element.dataset.status;
+        },
+        set status(value) {
+          this.element.dataset.status = value;
+        },
+      };
+      row.push(tile);
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+export function markTile(tile) {
+  if (
+    tile.status !== TILE_STATUSES.HIDDEN &&
+    tile.status !== TILE_STATUSES.MARKED
+  ) {
+    return;
+  }
+  if (tile.status === TILE_STATUSES.MARKED) {
+    tile.status = TILE_STATUSES.HIDDEN;
+  } else {
+    tile.status = TILE_STATUSES.MARKED;
+  }
+}
+
+export function revealTile(board, tile) {
+  if (tile.status !== TILE_STATUSES.HIDDEN) {
+    return;
+  }
+  if (tile.mine) {
+    tile.status = TILE_STATUSES.MINE;
+    return;
+  }
+  tile.status = TILE_STATUSES.NUMBER;
+  const adjacentTiles = nearbyTiles(board, tile);
+  const mines = adjacentTiles.filter((tile) => tile.mine);
+  if (mines.length === 0) {
+    adjacentTiles.forEach(revealTile.bind(null, board));
+  } else {
+    tile.element.textContent = mines.length;
+  }
+}
+
+export function checkWin(board) {
+  return board.every((row) => {
+    return row.every((tile) => {
+      return (
+        tile.status === TILE_STATUSES.NUMBER ||
+        (tile.mine &&
+          (tile.status === TILE_STATUSES.HIDDEN ||
+            tile.status === TILE_STATUSES.MARKED))
+      );
+    });
+  });
+}
+
+export function checkLose(board) {
+  return board.some((row) => {
+    return row.some((tile) => {
+      return tile.status === TILE_STATUSES.MINE;
+    });
+  });
+}
+
+function getMinePosition(boarSize, numberOfMines) {
+  const positions = [];
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boarSize),
+      y: randomNumber(boarSize),
+    };
+    if (!positions.some((p) => positionMatch(p, position))) {
+      positions.push(position);
+    }
+  }
+  return positions;
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+function nearbyTiles(board, { x, y }) {
+  const tiles = [];
+  for (let xOffSet = -1; xOffSet <= 1; xOffSet++) {
+    for (let yOffSet = -1; yOffSet <= 1; yOffSet++) {
+      const tile = board[x + xOffSet]?.[y + yOffSet];
+      if (tile) tiles.push(tile);
+    }
+  }
+
+  return tiles;
+}
